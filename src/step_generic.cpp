b378447223fdf7c41a472bb74866909a512a434b
PMack10 <105826142+PMack10@users.noreply.github.com>
2025-04-30 20:17:16 +0100

    Update step_generic.cpp

--- src/step_generic.cpp
+++ src/step_generic.cpp
@@ -985,7 +985,7 @@ void step_update_EDHB_NL(RPR f, RPR f_2, RPR f_3, component fc, const grid_volum
 
                     if (chi2new[i] == 0.0) { continue; }// 
 
-                      if (25 + (i % 300) == 0) {
+                      if (38 + (i % 300) == 0) {
                       cout << "at entry point" << fw_2_atZ[i] << "    " << fw_3_atZ[i] << "     "
                            << f[i] << endl;
                       cout << u[i] << "    " << u_2[i] << "     " << u_2[i + s] << "   " << u_3[i]
