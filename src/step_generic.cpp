79cf7b063c1e09478b37e62c6828fc6ca1d19228
PMack10 <105826142+PMack10@users.noreply.github.com>
2025-05-07 00:05:16 +0100

    Update step_generic.cpp

--- src/step_generic.cpp
+++ src/step_generic.cpp
@@ -604,6 +604,7 @@ void step_update_EDHB(RPR f, component fc, const grid_volume &gv, const ivec is,
     KSTRIDE_DEF(dsigw, kw, is, gv);
     if (u1 && u2) { // 3x3 off-diagonal u
       cout << " shouldn't be in pml u1&u2.." << endl;
+      sleep(20);
       if (chi3) {
 
          cout << " shouldn't be in pml chi3..." << endl;
@@ -747,43 +748,44 @@ void step_update_EDHB(RPR f, component fc, const grid_volume &gv, const ivec is,
       realnum gs_2 = (g1[i] + g1[i + s] + g1[i - s1] + g1[i + (s - s1)]) * 0.25; // dmpY at X locations (when fc == ex)
       realnum gs_3 = (g2[i] + g2[i + s] + g2[i - s2] + g2[i + (s - s2)]) * 0.25; // dmpZ at X locations
 
-      if (u[i] == 0 || u_2[i] == 0 || u_3[i] == 0) {
-            cout << "u is zero!! " << u[i] << "  " << u_2[i] << "  " << u_3[i] << endl;
+      if (u[i] == 0 || u1[i] == 0 || u2[i] == 0) {
+            cout << "u is zero!! " << u[i] << "  " << u1[i] << "  " << u2[i] << endl;
             sleep(5);
           }
 
           /// taking inverse of chi1inverse is easiest way to access epsilon...
           realnum us = 1 / u[i];
-          realnum us_2 = 1 / (u_2[i]);
-          realnum us_3 = 1 / u_3[i];
+          realnum us_2 = 1 / (u1[i]);
+          realnum us_3 = 1 / (u2[i]);
           realnum dummyF1 = 0.0;
           realnum dummyF2 = 0.0;
+          realnum chi2new = chi2[i];
 
           if (fc == 0) { // Ex
             // will be format Parameters p1 = {prevF D-P_X, eps, 0, 0, 0, chi2new, 0, 0 } etc;
-            Parameters p1 = {gs, us, 0.0, 0.0, 0.0, chi2new[i], 0.0, 0.0}; // X
-            Parameters p2 = {gs_2, us_2, 0.0, 0.0, 0.0, 0.0, chi2new[i], 0.0}; // Y
-            Parameters p3 = {gs_3,  us_3,  0.0, 0.0, 0.0, 0.0, 0.0, chi2new[i]}; // Z.
+            Parameters p1 = {gs, us, 0.0, 0.0, 0.0, chi2new, 0.0, 0.0}; // X
+            Parameters p2 = {gs_2, us_2, 0.0, 0.0, 0.0, 0.0, chi2new, 0.0};    // Y
+            Parameters p3 = {gs_3,  us_3,  0.0, 0.0, 0.0, 0.0, 0.0, chi2new}; // Z.
             realnum seed1 = f[i];
-            realnum seed2 = gs_2 * u_2[i];
-            realnum seed3 = gs_3 * u_3[i];
+            realnum seed2 = gs_2 * u1[i];
+            realnum seed3 = gs_3 * u2[i];
             runNR(seed1, seed2, seed3, &f[i], &dummyF1, &dummyF2, p1, p2, p3);
           }
           else if (fc == 1) { //Ey
-            Parameters p1 = {gs_3, us_3, 0.0, 0.0, 0.0, chi2new[i], 0.0, 0.0};     // X
-            Parameters p2 = {gs, us, 0.0, 0.0, 0.0, 0.0, chi2new[i], 0.0}; // Y
-            Parameters p3 = {gs_2, us_2, 0.0, 0.0, 0.0, 0.0, 0.0, chi2new[i]}; // Z.
-            realnum seed1 = gs_3 * u_3[i];
+            Parameters p1 = {gs_3, us_3, 0.0, 0.0, 0.0, chi2new, 0.0, 0.0};     // X
+            Parameters p2 = {gs, us, 0.0, 0.0, 0.0, 0.0, chi2new, 0.0}; // Y
+            Parameters p3 = {gs_2, us_2, 0.0, 0.0, 0.0, 0.0, 0.0, chi2new}; // Z.
+            realnum seed1 = gs_3 * u2[i];
             realnum seed2 = f[i];
-            realnum seed3 = gs_2 * u_2[i];
+            realnum seed3 = gs_2 * u1[i];
             runNR(seed1, seed2, seed3, &dummyF1, &f[i], &dummyF2, p1, p2, p3);
           }
           else if (fc == 4) { //Ez
-            Parameters p1 = {gs_2, us_2, 0.0, 0.0, 0.0, chi2new[i], 0.0, 0.0}; // X
-            Parameters p2 = {gs_3, us_3, 0.0, 0.0, 0.0, 0.0, chi2new[i], 0.0}; // Y
-            Parameters p3 = {gs, us, 0.0, 0.0, 0.0, 0.0, 0.0, chi2new[i]}; // Z.
-            realnum seed1 = gs_2 * u_2[i];
-            realnum seed2 = gs_3 * u_3[i];
+            Parameters p1 = {gs_2, us_2, 0.0, 0.0, 0.0, chi2new, 0.0, 0.0}; // X
+            Parameters p2 = {gs_3, us_3, 0.0, 0.0, 0.0, 0.0, chi2new, 0.0}; // Y
+            Parameters p3 = {gs, us, 0.0, 0.0, 0.0, 0.0, 0.0, chi2new}; // Z.
+            realnum seed1 = gs_2 * u1[i];
+            realnum seed2 = gs_3 * u2[i];
             realnum seed3 = f[i];
             runNR(seed1, seed2, seed3, &dummyF1, &dummyF1, &f[i], p1, p2, p3);
           }
