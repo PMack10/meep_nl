57454bf5e6fbdb092313fd88ca709594c652ffeb
PMack10 <105826142+PMack10@users.noreply.github.com>
2025-05-07 11:02:04 +0100

    Update step_generic.cpp

--- src/step_generic.cpp
+++ src/step_generic.cpp
@@ -740,15 +740,15 @@ void step_update_EDHB(RPR f, component fc, const grid_volume &gv, const ivec is,
           realnum gs_3 =
               (g2[i] + g2[i + s] + g2[i - s2] + g2[i + (s - s2)]) * 0.25; // dmpZ at X locations
 
-          if (u[i] == 0 || u1[i] == 0 || u2[i] == 0) {
+          if (u[i] == 0){ // || u1[i] == 0 || u2[i] == 0) { (making all eps values equal for now, can change later)
             cout << "u is zero!! " << u[i] << "  " << u1[i] << "  " << u2[i] << endl;
             sleep(5);
           }
 
           /// taking inverse of chi1inverse is easiest way to access epsilon...
           realnum us = 1 / u[i];
-          realnum us_2 = 1 / (u1[i]);
-          realnum us_3 = 1 / (u2[i]);
+          realnum us_2 = us // 1 / (u1[i]);
+          realnum us_3 = us //1 / (u2[i]);  (making all eps values equal for now, can change later)
           realnum dummyF1 = 0.0;
           realnum dummyF2 = 0.0;
           realnum chi2new = chi2[i];
@@ -759,25 +759,25 @@ void step_update_EDHB(RPR f, component fc, const grid_volume &gv, const ivec is,
             Parameters p2 = {gs_2, us_2, 0.0, 0.0, 0.0, 0.0, chi2new, 0.0}; // Y
             Parameters p3 = {gs_3, us_3, 0.0, 0.0, 0.0, 0.0, 0.0, chi2new}; // Z.
             realnum seed1 = f[i];
-            realnum seed2 = gs_2 * u1[i];
-            realnum seed3 = gs_3 * u2[i];
+            realnum seed2 = gs_2 * u[i];// * u1[i];
+            realnum seed3 = gs_3 * u[i]; //  * u2[i];
             runNR(seed1, seed2, seed3, &f[i], &dummyF1, &dummyF2, p1, p2, p3);
           }
           else if (fc == 1) {                                               // Ey
             Parameters p1 = {gs_3, us_3, 0.0, 0.0, 0.0, chi2new, 0.0, 0.0}; // X
             Parameters p2 = {gs, us, 0.0, 0.0, 0.0, 0.0, chi2new, 0.0};     // Y
             Parameters p3 = {gs_2, us_2, 0.0, 0.0, 0.0, 0.0, 0.0, chi2new}; // Z.
-            realnum seed1 = gs_3 * u2[i];
+            realnum seed1 = gs_3 * u[i];//  * u2[i];
             realnum seed2 = f[i];
-            realnum seed3 = gs_2 * u1[i];
+            realnum seed3 = gs_2 * u[i];//   * u1[i];
             runNR(seed1, seed2, seed3, &dummyF1, &f[i], &dummyF2, p1, p2, p3);
           }
           else if (fc == 4) {                                               // Ez
             Parameters p1 = {gs_2, us_2, 0.0, 0.0, 0.0, chi2new, 0.0, 0.0}; // X
             Parameters p2 = {gs_3, us_3, 0.0, 0.0, 0.0, 0.0, chi2new, 0.0}; // Y
             Parameters p3 = {gs, us, 0.0, 0.0, 0.0, 0.0, 0.0, chi2new};     // Z.
-            realnum seed1 = gs_2 * u1[i];
-            realnum seed2 = gs_3 * u2[i];
+            realnum seed1 = gs_2  * u[i];// * u1[i];
+            realnum seed2 = gs_3 * u[i];//  * u2[i];
             realnum seed3 = f[i];
             runNR(seed1, seed2, seed3, &dummyF1, &dummyF1, &f[i], p1, p2, p3);
           }
